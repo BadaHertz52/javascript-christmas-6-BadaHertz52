7f56253ec78f01df44f37d14ed94682264a5cb51
badahertz52 <badahertz52@gmail.com>
2023-11-12 19:22:07 +0900

    docs<Message>: QUERY  속성명 변경
    - reservation -> reservationDate
    - menu-> order

--- src/constants/Message.js
+++ src/constants/Message.js
@@ -20,9 +20,10 @@ const ERROR_MESSAGE = freezeObject({
 });
 
 const QUERY = freezeObject({
-  reservation:
+  reservationDate:
     '12월 중 식당 예상 방문 날짜는 언제인가요? (1~31의 숫자만 입력해 주세요!)',
-  menu: '주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+  order:
+    '주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
 });
 
 const MESSAGE = freezeObject({

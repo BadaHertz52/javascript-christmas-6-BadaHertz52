f32a7fe88f093b6f51235ee05f6809c34492a446
badahertz52 <badahertz52@gmail.com>
2023-11-14 16:28:20 +0900

    refactor : 테스트에 반복적으로 사용하는 함수들을 testUtils/index.js로 옮김

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,33 +1,6 @@
 import App from "../src/App.js";
-import { MissionUtils } from "@woowacourse/mission-utils";
-import { EOL as LINE_SEPARATOR } from "os";
-
-const mockQuestions = (inputs) => {
-  MissionUtils.Console.readLineAsync = jest.fn();
-
-  MissionUtils.Console.readLineAsync.mockImplementation(() => {
-    const input = inputs.shift();
-
-    return Promise.resolve(input);
-  });
-};
-
-const getLogSpy = () => {
-  const logSpy = jest.spyOn(MissionUtils.Console, "print");
-  logSpy.mockClear();
-
-  return logSpy;
-};
-
-const getOutput = (logSpy) => {
-  return [...logSpy.mock.calls].join(LINE_SEPARATOR);
-};
-
-const expectLogContains = (received, expectedLogs) => {
-  expectedLogs.forEach((log) => {
-    expect(received).toContain(log);
-  });
-};
+import { EOL as LINE_SEPARATOR } from 'os';
+import { mockQuestions, getLogSpy, getOutput, expectLogContains } from '../testUtils/index.js'
 
 describe("기능 테스트", () => {
   test("모든 타이틀 출력", async () => {
---
+++ testUtils/index.js
@@ -0,0 +1,31 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+import { EOL as LINE_SEPARATOR } from 'os';
+
+const mockQuestions = (inputs) => {
+  MissionUtils.Console.readLineAsync = jest.fn();
+
+  MissionUtils.Console.readLineAsync.mockImplementation(() => {
+    const input = inputs.shift();
+
+    return Promise.resolve(input);
+  });
+};
+
+const getLogSpy = () => {
+  const logSpy = jest.spyOn(MissionUtils.Console, 'print');
+  logSpy.mockClear();
+
+  return logSpy;
+};
+
+const getOutput = (logSpy) => {
+  return [...logSpy.mock.calls].join(LINE_SEPARATOR);
+};
+
+const expectLogContains = (received, expectedLogs) => {
+  expectedLogs.forEach((log) => {
+    expect(received).toContain(log);
+  });
+};
+
+export { mockQuestions, getLogSpy, getOutput, expectLogContains };
